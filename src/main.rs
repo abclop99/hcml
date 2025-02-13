cfac09b5cd725242ed5d834feed51b898815eeb2
Aaron Li <aaron.li@genvira.com>
2025-02-13 15:43:34 -0500

    🚧 Add pest grammar and an example file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use pest::Parser;
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "hbml.pest"]
+struct HbmlParser;
+
 fn main() {
-    println!("Hello, world!");
+    let document = include_str!("../test/example-1.hbml");
+
+    let result = HbmlParser::parse(Rule::document, document);
+
+    match result {
+        Ok(pairs) => println!("Parsed successfully: {pairs:?}"),
+        Err(e) => println!("Parse failed: {e}"),
+    }
 }
