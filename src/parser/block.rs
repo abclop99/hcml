922e2f9cd263f01631a860564b39d75a9d1fa11c
Aaron Li <aaron.li@genvira.com>
2025-02-24 13:14:54 -0500

    🐛 (comments): Fix comments in block (node list)

--- src/parser/block.rs
+++ src/parser/block.rs
@@ -5,7 +5,7 @@ use pest::iterators::Pairs;
 
 use crate::{
     html::node::NodeList,
-    parser::{create_node, Rule},
+    parser::{create_comment, create_node, Rule},
 };
 
 /// Handles a HBML block
@@ -21,15 +21,29 @@ pub(crate) fn create_block(inner: Pairs<Rule>) -> NodeList {
     let mut node_list = NodeList::new();
 
     for pair in inner {
-        let node = create_node(pair.into_inner());
+        match pair.as_rule() {
+            Rule::node => {
+                let node = create_node(pair.into_inner());
 
-        // Box the node if necessary
-        let node = match node {
-            Either::Left(node) => node,
-            Either::Right(node_list) => Box::new(node_list),
-        };
+                // Box the node if necessary
+                let node = match node {
+                    Either::Left(node) => node,
+                    Either::Right(node_list) => Box::new(node_list),
+                };
 
-        node_list.push(node);
+                node_list.push(node);
+            }
+            Rule::COMMENT => {
+                let comment = Box::new(create_comment(pair.into_inner()));
+
+                node_list.push(comment);
+            }
+            _ => unimplemented!(
+                "Unexpected rule {:?} found: {:?}",
+                pair.as_rule(),
+                pair.as_span(),
+            ),
+        }
     }
 
     node_list
